d55a5185189e2d38358112b06785878a64182343
makarov <sdf.brain@gmail.com>
2016-05-24 22:02:43 +0300

    create classdemo.rb

---
+++ classdemo.rb
@@ -0,0 +1,10 @@
+class Song
+	attr_accessor :name, :duration
+	def initialize name, duration
+		@name = name
+		@duration = duration
+
+	end
+end
+song1 = Song.new "Show must go on", 6
+puts song1.name
